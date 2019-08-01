b4bb2f4491b801fce2fd45c991d3a1c1dd0632e7
samin36 <43042747+samin36@users.noreply.github.com>
2019-08-01 08:46:05 -0400

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import json
 
 login_info = {
     'url': "https://hacprd.fultonschools.org/HomeAccess/Account/LogOn?ReturnUrl=%2fHomeAccess%2f",
-    'username': '2000004079',
-    'password': '04272005'
+    'username': '[HAC Username]',
+    'password': '[HAC Password]'
 }
